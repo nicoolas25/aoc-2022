51e7a5d2508e5f57f55a921865b5ee445dbb685e
Nicolas Zermati <nicoolas25@gmail.com>
2022-12-11 17:54:20 +0100

    Simplify day11 with regexp and drop Item structure

--- day11/day11.rb
+++ day11/day11.rb
@@ -1,46 +1,31 @@
-OPERATORS = %w[+ - * /].freeze
-
-Item = Struct.new(
-  :id,
-  :worry_level
-)
-
-Monkey = Struct.new(
-  :name,
-  :items,
-  :inspection_op,
-  :divisible_by,
-  :routing_fn,
-  :inspection_counter
-)
-
-def Monkey.parse(*args)
-  name, items, operation, test, if_true, if_false = args
-  name = name[7]
-  items = items.scan(/\d+/).map.with_index do |worry_level, i|
-    Item.new("#{name}_#{i}", worry_level.to_i)
+Monkey = Struct.new(:name, :items, :do_inspect, :multiple, :if_true, :if_false, :inspect_count) do
+  def inspect_first_item!
+    self.inspect_count += 1
+    do_inspect.call(items.shift)
   end
-  inspection_op =
-    case operation[23..].split
-    in [operator, "old"] if OPERATORS.include?(operator)
-      -> { _1.worry_level.public_send(operator, _1.worry_level) }
-    in [operator, operand] if OPERATORS.include?(operator)
-      -> { _1.worry_level.public_send(operator, operand.to_i) }
-    in any
-      raise "Can't handle #{any}"
-    end
-  divisible_by = test.scan(/\d+/).first.to_i
-  routing_fn = -> {
-    (_1.worry_level % divisible_by).zero? ? if_true[29] : if_false[30]
-  }
-  Monkey.new(
-    name,
-    items,
-    inspection_op,
-    divisible_by,
-    routing_fn,
-    0
-  )
+
+  def successor_for(item)
+    (item % multiple).zero? ? if_true : if_false
+  end
+end
+
+def Monkey.parse(lines)
+  matches = lines.match(Regexp.new(
+    'Monkey (?<name>\d):' \
+    '\s+Starting items: (?<items>\d+(, \d+)*)' \
+    '\s+Operation: new = old (?<operator>[+*]) (?<operand>\d+|old)' \
+    '\s+Test: divisible by (?<multiple>\d+)' \
+    '\s+If true: throw to monkey (?<if_true>\d)' \
+    '\s+If false: throw to monkey (?<if_false>\d)'
+  ))
+  name, if_true, if_false = matches.values_at(:name, :if_true, :if_false)
+  items = matches[:items].split(", ").map(&:to_i)
+  multiple = matches[:multiple].to_i
+  do_inspect = case matches.values_at(:operator, :operand)
+               in [op, "old"] then -> { _1.public_send(op, _1) }
+               in [op, i] then -> { _1.public_send(op, i.to_i) }
+               end
+  Monkey.new(name, items, do_inspect, multiple, if_true, if_false, 0)
 end
 
 monkeys_by_name = ARGF
@@ -48,22 +33,21 @@ monkeys_by_name = ARGF
   .map(&:chomp)
   .reject(&:empty?)
   .each_slice(6)
-  .map { Monkey.parse(*_1) }
+  .map { Monkey.parse(_1.join("\n")) }
   .map { [_1.name, _1] }
   .to_h
 
-lcm = monkeys_by_name.values.map(&:divisible_by).reduce { _1.lcm(_2) }
+
+lcm = monkeys_by_name.values.map(&:multiple).reduce { _1.lcm(_2) }
 
 10_000.times do
   monkeys_by_name.each_value do |monkey|
     monkey.items.size.times do
-      item = monkey.items.shift
-      item.worry_level = monkey.inspection_op.call(item) % lcm
-      monkeys_by_name.fetch(monkey.routing_fn.call(item)).items << item
-      monkey.inspection_counter += 1
+      item = monkey.inspect_first_item! % lcm
+      monkeys_by_name.fetch(monkey.successor_for(item)).items << item
     end
   end
 end
 
-top1, top2 = monkeys_by_name.values.map(&:inspection_counter).sort.last(2)
+top1, top2 = monkeys_by_name.values.map(&:inspect_count).sort.last(2)
 puts top1 * top2
